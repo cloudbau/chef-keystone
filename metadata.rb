c83c4cab85db5ae51cfe25f7a10f6fa709d05a74
m.benchchaoui@cloudbau.de <m.benchchaoui@cloudbau.de>
2013-11-06 12:09:23 +0100

    Change cookbook version

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,8 @@
-maintainer       "Opscode, Inc."
-maintainer_email "matt@opscode.com"
+maintainer       "Cloudbau GmbH"
 license          "Apache 2.0"
 description      "The OpenStack Identity service Keystone."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "5.0.1"
+version          "6.0.0"
 
 %w{ ubuntu fedora }.each do |os|
   supports os
